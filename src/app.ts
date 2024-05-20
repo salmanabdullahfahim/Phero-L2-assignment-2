1632c4f6b65119ebaa3b54bc2130d4fe9016ee56
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-05-20 19:53:07 +0600

    product post route created

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express, { Request, Response } from 'express';
+import { productRoutes } from './modules/product/product.route';
 const app = express();
 
 //parsers
 app.use(express.json());
+
+//routes
+app.use('/api/products', productRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
