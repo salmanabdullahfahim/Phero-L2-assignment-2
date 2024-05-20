49aecc849c80e15016afab8f48c8b5edfccc2de0
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-05-20 19:49:55 +0600

    controller- create product added

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,3 +1,20 @@
 import { Request, Response } from 'express';
+import { productServices } from './product.service';
 
-const createProduct = async (req: Request, res: Response) => {};
+const createProduct = async (req: Request, res: Response) => {
+  try {
+    const productData = req.body;
+    const result = await productServices.createProductIntoDb(productData);
+    res.status(200).json({
+      success: true,
+      message: 'Product created successfully!',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const productControllers = {
+  createProduct,
+};
