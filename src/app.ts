df175a9ae8e0393bca72456043268a514aa1224b
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-05-21 07:43:33 +0600

    handle route not found

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,12 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+// handle route not found
+app.use((req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: 'Route not found',
+  });
 });
 
 export default app;
